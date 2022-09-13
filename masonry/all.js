7fb4ef716be624a26fa58dda0d3028c2b1b999ee
Rebecca <rebecca.yu@senao.com.tw>
2022-09-13 14:29:45 +0800

    css animation and some js

--- masonry/all.js
+++ masonry/all.js
@@ -1,10 +1,11 @@
-const grid = document.querySelector(".grid");
+const grid = document.querySelector('.grid');
 
 const masonry = new Masonry(grid, {
-  itemSelector: ".grid-item",
-  columnWidth: ".grid-sizer",
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true
 });
 
-imagesLoaded(grid).on("progress", function () {
+imagesLoaded(grid).on('progress', function () {
   masonry.layout();
 });
