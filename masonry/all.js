cf6eb109b2fb68729a34f68fc1b26ce37abaf4b8
rebecca <rebecca.yu@senao.com.tw>
2022-09-13 20:04:56 +0800

    完成彈跳載入動畫

--- masonry/all.js
+++ masonry/all.js
@@ -1,11 +1,27 @@
 const grid = document.querySelector('.grid');
+const items = document.querySelectorAll(".grid-item");
+let index = 0;
 
 const masonry = new Masonry(grid, {
   itemSelector: '.grid-item',
-  columnWidth: '.grid-sizer',
-  percentPosition: true
+  columnWidth: '.grid-sizer'
 });
 
-imagesLoaded(grid).on('progress', function () {
-  masonry.layout();
+// Triggered after all images have successfully loaded without any broken images.
+imagesLoaded(grid).on('done', function () {
+  window.requestAnimationFrame(bounceInImages);
+});
+
+const bounceInImages = () => {
+  items[index].classList.add('bounce-in');
+  index ++;
+  if (index < items.length) {
+    // 要自己call自己才有下一次
+    window.requestAnimationFrame(bounceInImages);
+  }
+};
+
+// css 動畫完成後觸發animationend
+grid.addEventListener('animationend', (e) => {
+  e.target.classList.remove('hide');
 });
